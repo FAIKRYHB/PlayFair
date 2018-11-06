d44e098c2fcd9987231eff8dbdc17c43e1df0e10
Jan Hošek <j_hosek@utb.cz>
2018-11-06 21:56:44 +0100

    Update main.py

--- main.py
+++ main.py
@@ -2,15 +2,43 @@ rawinstring="ab.Cč d Eěf#GiÍJ  KL:&mnňoó pQrŘsšTťUúův wxYzž"
 friedinstring=""
 reple = [["a","á","ä","Á","Ä"],["b"],["č","ć","c","Č","Ć"],["d","ď","Ď"],["ě","é","e","ë","Ě","É","Ë"],["f"],["g"],["h"],["i","í","ï","Í","Ï"],["j"],["k"],["l","ĺ","Ĺ"],["m"],["n","ň","ń","Ň","Ń"],["o","ó","ö","Ó","Ö"],["p"],["q"],["r","ř","ŕ","Ř","Ŕ"],["š","ś","s","Š","Ś"],["t","ť","Ť"],["u","ú","ů","Ů","Ú"],["v"],["w"],["x"],["y","ý","ÿ","Ý","Ÿ"],["z","ž","ź","Ž","Ź"]]
 forThing = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
+lang="W"
 
 for i in range(0,len(rawinstring)):
     if rawinstring[i]<"A" or rawinstring[i]>"Z":
         if rawinstring[i]==" ":
             friedinstring+="MEZERA"
         else:
-            for j in range(0, len(reple)):
-                if rawinstring[i] in reple[j]:
-                    #print(forThing[j])
-                    friedinstring+=forThing[j]
+            if rawinstring[i]==chr(lang+32):
+                if lang=="w":
+                    friedinstring+="V"
+                if lang=="j":
+                    friedinstring+="I"
+            else:
+                for j in range(0, len(reple)):
+                    if rawinstring[i] in reple[j]:
+                        #print(forThing[j])
+                        friedinstring+=forThing[j]
     else:
-        friedinstring+=rawinstring[i]
+        if rawinstring[i]==lang:
+            if lang=="W":
+                friedinstring+="V"
+            if lang=="J":
+                friedinstring+="I"
+        else:
+            friedinstring+=rawinstring[i]
+        
+key="bananovnik" #baovik
+table=""
+i=0
+
+while i < 25:
+    if i>=len(key):
+        for j in range(0,25):
+            if not(65+j in table):
+                table+=chr(65+j)
+                i+=1
+    else:
+        if not(key[i] in table):
+            table+=key[i]
+            i+=1
